f24a18d582d626e6585d7e1e8f44694506e8d46d
rose-goldy <rosecgoldenberg@gmail.com>
2023-03-08 09:38:59 -0600

    Initial Commit

---
+++ AP App/AP_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AP_AppApp.swift
+//  AP App
+//
+//  Created by Rose Goldenberg23 on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct AP_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
