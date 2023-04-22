e1c806d0acdc8ba35bbed53c9e55f1c0f447aa6f
rose-goldy <rosecgoldenberg@gmail.com>
2023-04-22 12:41:53 -0500

    saturday progress

--- AP App/Views/WorldShortRankings.swift
+++ AP App/Views/WorldShortRankings.swift
@@ -1,58 +1,68 @@
-////
-////  WorldShortRankings.swift
-////  AP App
-////
-////  Created by Rose Goldenberg23 on 4/17/23.
-////
 //
-//import SwiftUI
+//  WorldShortRankings.swift
+//  AP App
 //
-////my plan is just to have the list of junior worlds 2023 and then try to insert the person's (user's) score in order
-////(this will be my function)
+//  Created by Rose Goldenberg23 on 4/17/23.
 //
-////ROSE REFER BACK TO NATIONAL PARKS SHIT WITH ALL THE LISTS
-//
-//
-//struct WorldShortRankings: View {
-//    var body: some View {
-//        
-//        NavigationView{
-//            
-//            List {
-//                
-//                ForEach (0...worldTeamList.count-1, id: \.self) { index in
-//                    
-//                    let oneTeamData: [String] = [worldTeamList[index], String(worldScoreList[index]), worldTeamAndScoreList[index]]
-//                    
-//                    //      NavigationLink(destination:OneTeamView
-//                    //                          (oneTeamData: oneTeamData)) {
-//                    
-//                    VStack{ 
-//                        
-//                        Group {
-//                            Text(oneTeamData[0])
-//                            
-//                            Text(oneTeamData[1])
-//                        }
-//                        
-//                        
-//                        OneTeamView()
-//                    }
-//                    
-//                }
-//                
-//            }
-//            .frame(maxWidth: .infinity, alignment: .leading)
-//            .padding()
-//            .background()
-//            .cornerRadius(16)
-//            .shadow(color: .gray, radius: 8, x:4, y:4)
-//        }
-//    }
-//}
-//
-//struct WorldShortRankings_Previews: PreviewProvider {
-//    static var previews: some View {
-//        WorldShortRankings()
-//    }
-//}
+
+import SwiftUI
+
+//my plan is just to have the list of junior worlds 2023 and then try to insert the person's (user's) score in order
+//(this will be my function)
+
+//ROSE REFER BACK TO NATIONAL PARKS SHIT WITH ALL THE LISTS
+
+
+struct WorldShortRankings: View {
+    var body: some View {
+        
+        NavigationView{
+            
+            List {
+                
+                Text("World Junior Short Ranking")
+                    .ignoresSafeArea()
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .frame(width: 6000, height: 80)
+                    .background(Color.blue)
+                
+                ForEach (0...worldTeamList.count-1, id: \.self) { index in
+                    
+           //         let oneTeamData: [String] = [worldTeamList[index], String(worldScoreList[index]), worldTeamAndScoreList[index]]
+                    
+                    //      NavigationLink(destination:OneTeamView
+                    //                          (oneTeamData: oneTeamData)) {
+                    
+                        
+                        HStack {
+                           Text("\(worldTeamList[index])")
+                                .fontWeight(.bold)
+                                .font(.title2)
+                                .padding(20)
+                            
+                            Text("\(worldScoreList[index], specifier: "%.2f")")
+                                .fontWeight(.bold)
+                                .font(.title2)
+                                .padding(20)
+                            
+                        }
+                    
+                }
+                
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding()
+            .background()
+            .cornerRadius(16)
+            .shadow(color: .gray, radius: 8, x:4, y:4)
+            .listStyle(.plain)
+        }
+    }
+}
+
+struct WorldShortRankings_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldShortRankings()
+    }
+}
